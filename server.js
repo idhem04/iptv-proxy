c95cd325d48b4779ae31cd70e563a2182fa44fde
idhem04 <44610872+idhem04@users.noreply.github.com>
2025-02-08 18:47:45 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ app.use(cors()); // إزالة قيود CORS
 const channels = [
     { name: "AlAoula", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83728.ts" },
     { name: "2M", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83727.ts" },
-    { name: "N24", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83726.ts" }
+    { name: "AlMaghribia", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83729.ts" },
+    { name: "MD1Arabic", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83731.ts" },
+    { name: "MD1Maghreb", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83732.ts" },
+    { name: "MD1Afriq", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83733.ts" },
+    { name: "Laayoun", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83734.ts" },
+    { name: "M24", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83730.ts" }
 ];
 
 // صفحة رئيسية تعرض القنوات
