a908a209ac8bbb26068dbae725d0a9c20144d65a
idhem04 <44610872+idhem04@users.noreply.github.com>
2025-02-08 19:23:03 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,14 +7,34 @@ app.use(cors()); // إزالة قيود CORS
 
 // قائمة القنوات
 const channels = [
-    { name: "AlAoula", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83728.ts" },
-    { name: "2M", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83727.ts" },
+    { name: "AlAoula", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83727.ts" },
+    { name: "2M", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83728.ts" },
     { name: "AlMaghribia", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83729.ts" },
-    { name: "MD1Arabic", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83731.ts" },
-    { name: "MD1Maghreb", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83732.ts" },
-    { name: "MD1Afriq", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83733.ts" },
-    { name: "Laayoun", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83734.ts" },
-    { name: "M24", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83730.ts" }
+    { name: "MD1Arabic", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83730.ts" },
+    { name: "MD1Maghreb", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83731.ts" },
+    { name: "MD1Afriq", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83732.ts" },
+    { name: "Laayoun", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83733.ts" },
+    { name: "AlAoula", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83734.ts" },
+    { name: "2M", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83735.ts" },
+    { name: "AlMaghribia", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83736.ts" },
+    { name: "MD1Arabic", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83737.ts" },
+    { name: "MD1Maghreb", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83738.ts" },
+    { name: "MD1Afriq", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83739.ts" },
+    { name: "Laayoun", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83740.ts" },
+    { name: "AlAoula", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83741.ts" },
+    { name: "2M", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83742.ts" },
+    { name: "AlMaghribia", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83743.ts" },
+    { name: "MD1Arabic", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83744.ts" },
+    { name: "MD1Maghreb", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83745.ts" },
+    { name: "MD1Afriq", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83746.ts" },
+    { name: "Laayoun", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83747.ts" },
+    { name: "AlAoula", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83748.ts" },
+    { name: "2M", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83749.ts" },
+    { name: "AlMaghribia", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83750.ts" },
+    { name: "MD1Arabic", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83751.ts" },
+    { name: "MD1Maghreb", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83752.ts" },
+    { name: "MD1Afriq", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83753.ts" },
+    { name: "Laayoun", url: "http://app.upsdo.me:8080/live/PCYXRYCVGSBR/718188917877/83754.ts" },
 ];
 
 // صفحة رئيسية تعرض القنوات
